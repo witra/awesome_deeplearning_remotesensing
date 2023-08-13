7e988018d1078e8008884468c0597b8d267370fa
witra <tahjudil.witra@gmail.com>
2023-08-13 17:15:18 +0700

    make it fail, to test the branck protection

--- Tests/tests_data/test_get_rapidai4eo.py
+++ Tests/tests_data/test_get_rapidai4eo.py
@@ -31,8 +31,8 @@ def fixture_RapidAI4EO_data2():
 @pytest.fixture(scope='class', name='geom_RapidAI4EO_data2')
 def fixture_geom_RapidAI4EO_data2(RapidAI4EO_data2):
     directory = "../data_test/"
-    if not os.path.exists(directory):
-        os.mkdir(directory)
+    # if not os.path.exists(directory):
+    #     os.mkdir(directory)
     RapidAI4EO_data2.get_geometries(path=f"{directory}rapidai4eo_geometries.geojson.gz")
     return RapidAI4EO_data2.load_geometries()
 
