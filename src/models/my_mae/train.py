61c83b39ea8e3d79d53e741f82d4e9ef4610f3ef
witra <tahjudil.witra@gmail.com>
2023-09-12 07:31:21 +0700

    improve config arguments

--- src/models/my_mae/train.py
+++ src/models/my_mae/train.py
@@ -1,5 +1,5 @@
-import time
-from datetime import datetime
+import argparse
+import os
 from functools import partial
 
 import pandas as pd
@@ -7,33 +7,44 @@ import shapely
 import torch
 import torch.nn
 import torch.nn as nn
-import timm.optim.optim_factory as optim_factory
 import wandb
 from torch.utils.tensorboard import SummaryWriter
-
+from pathlib import Path
 from keys import wandb_props
 from src.data.get_RapidAI4EO import RapidAI4EO
 from src.models.mae.models_mae import MaskedAutoencoderViT
+from src import utils
 
-def main():
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Basic MAE trainer')
+    parser.add_argument('config_file', type=str, help="""
+    path to the config file
+    
+    """)
+    parser.add_argument('--verbose', default=True, type=bool, help='print some statements')
+
+    return parser
+
+def main(args):
     device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
-    best_loss = 1_000_000
+    best_loss = args.init_best_loss
 
     # 0. define dataset
-    aoi_geom = shapely.geometry.box(13.05, 52.35, 13.72, 52.69)
-    start_date = pd.to_datetime("2018-01-01T00:00:00Z")
-    end_date = pd.to_datetime("2019-01-01T00:00:00Z")
+    aoi_geom = shapely.geometry.box(args.lon_w, args.lat_s, args.lon_e, args.lat_n)
+    start_date = pd.to_datetime(args.start_date)
+    end_date = pd.to_datetime(args.end_date)
     time_stamp = (start_date, end_date)
 
     # 1. init and gather data from rapidai4eo project
     rapidaieo_data = RapidAI4EO(aoi_geom, date_range=time_stamp)
-    rapidaieo_data.get_geometries(path="./rapidai4eo_geometries.geojson.gz")
+    rapidaieo_data.get_geometries(path=args.planet_geomatry_path)
     geometries = rapidaieo_data.load_geometries()
     hrefs_planet = rapidaieo_data.filter_hrefs_on_geom(geometries=geometries, products=['pfsr'])
     planet_datapipe = rapidaieo_data.datapipe_img_only(hrefs_planet[:100],
-                                                       input_dims={'x': 64, 'y': 64},
-                                                       input_overlap={'x': 32, 'y': 32},
-                                                       batch_size=64
+                                                       input_dims={'x': args.x_input_dims, 'y': args.y_input_dims},
+                                                       input_overlap={'x': args.x_overlap, 'y': args.x_overlap},
+                                                       batch_size=args.batch_size
                                                        )
 
     # 2. Define data loader
@@ -43,13 +54,13 @@ def main():
 
     # 3. define  model
     model = MaskedAutoencoderViT(img_size=64, in_chans=4,
-                                 patch_size=8, embed_dim=60, depth=2, num_heads=12,
-                                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                                 patch_size=8, embed_dim=60, depth=args.attention_depth, num_heads=12,
+                                 decoder_embed_dim=512, decoder_depth=args.decoder_depth, decoder_num_heads=16,
                                  mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6))
     model.to(device)
 
     # 4. init loss, and optimizer
-    lr = 0.1
+    lr = args.lr
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, betas=(0.9, 0.95))
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
@@ -71,7 +82,7 @@ def main():
 
         for data_iter_step, batch in enumerate(data_loader_train):
             # move data to the specified device
-            batch.to(device)
+            batch = batch.to(device)
 
             # zero gradient per batch
             optimizer.zero_grad()
@@ -98,7 +109,7 @@ def main():
         if batch_loss.item()/data_iter_step < best_loss:
             print('save the model')
             best_loss = batch_loss.item()
-            model_path = '../../../models/my_mae/model_test_mae.pt'
+            model_path = os.path.join(args.output_dir, args.model_name)
             torch.save(model.state_dict(), model_path)
 
         # log data to
@@ -109,4 +120,11 @@ if __name__ == '__main__':
     """
     This script is to run the self-supervised experiment of MAE on the Planet dataset. 
     """
-    main()
+    args = get_args_parser()
+    args = args.parse_args()
+    if args.config_file:
+        config_path = args.config_file
+        config_data = utils.parse_config(config_path)
+        config_merge = utils.merge_config_with_parse(config_data, args)
+        args = argparse.Namespace(**config_merge)
+    main(args)
